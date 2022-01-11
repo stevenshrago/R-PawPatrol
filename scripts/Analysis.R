f426259501cdccba948f66abb27e91065ed1387d
stevenshrago <steven.shrago@gmail.com>
2022-01-11 14:43:04 +0100

    minor updates

--- scripts/Analysis.R
+++ scripts/Analysis.R
@@ -4,18 +4,18 @@ p_load(tidyverse, janitor, glue, extrafont, lubridate)
 
 data_raw <- readRDS("data_out/paw_patrol_db.rds")
 
-data_raw %>% tabyl(episode, season)
 
+data_raw
 
 #Pups appearances
 
-pups <- c("Zuma", "Skye", "Rubble", "Rocky", "Chase", "Marshall", "Ryder", "Robo-dog", "Everest", "Tracker", "Tuck", "Ella", "Rex", "Liberty")
+pups <- c("Zuma", "Skye", "Rubble", "Rocky", "Chase", "Marshall", "Ryder", "Robo-Dog", "Everest", "Tracker", "Tuck", "Ella", "Rex", "Liberty")
 
 (
   appearances <- data_raw %>%
     select(season, episode, overall, characters) %>%
     unnest(characters) %>%
-    filter(characters %in% pups) %>%
+    # filter(characters %in% pups) %>%
     count(characters) %>%
     # group_by(season) %>%
     slice_max(n, n = 30) %>%
@@ -36,12 +36,50 @@ cum_appearances <- data_raw %>%
   filter(characters %in% pups) %>%
   group_by(characters) %>% 
   mutate(appearance = 1,
-         cumsum = cumsum(appearance)) %>%
-  ungroup() %>% 
-  fill(c(overall, characters), .direction = "updown") %>%
+         cumsum = cumsum(appearance)) %>% 
+  select(overall, characters, cumsum) %>% 
   print()
+
 
 ggplot(cum_appearances, aes(x = overall, y = cumsum, group = characters)) +
   geom_line(aes(color = factor(characters)))
 
-fill
+
+  
+dff <-
+  tibble(characters = rep(
+    c(
+      "Zuma",
+      "Skye",
+      "Rubble",
+      "Rocky",
+      "Chase",
+      "Marshall",
+      "Ryder",
+      "Robo-dog",
+      "Everest",
+      "Tracker",
+      "Tuck",
+      "Ella",
+      "Rex",
+      "Liberty"
+    ),
+    times = 369
+  )) %>%
+  print()
+
+df <- tibble(overall = rep(1:369, times = 14)) %>%
+  arrange(overall) %>%
+  bind_cols(dff) %>%
+  left_join(cum_appearances %>%
+              mutate(overall = as.numeric(overall)),
+            by = c("overall", "characters")) %>%
+  mutate(cumsum = if_else(overall == 1 & is.na(cumsum), 0, cumsum)) %>%
+  group_by(characters) %>% 
+  fill(cumsum, .direction = "downup") %>%
+ view()
+
+
+appearances <- data_raw %>%
+  select(season, episode, overall, characters) %>%
+  unnest(characters)
